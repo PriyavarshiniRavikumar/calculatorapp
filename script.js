153a00b9e7cc13401b7d9d5bdd9c12ce9a8d90c0
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-10-22 14:27:30 +0530

    variables

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // JavaScript source code
 const display = document.querySelector("#res-box");
 const quesdisplay = document.querySelector("#ques-box");
+let Input = "";
+let Operator = "";
+let firstInput = 0;
+let result = 0;
