beba3c60152ae9896dc03538c0b13846f30fe7ae
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-10-23 10:04:11 +0530

    fetching numbers

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let Input = "";
 let Operator = "";
 let firstInput = 0;
 let result = 0;
+const numbers = document.querySelectorAll(".number");
+for (i = 0; i < numbers.length; i++) {
+    const operand = numbers[i];
+    operand.addEventListener("click", () => {
+        Input += operand.textContent;
+        display.textContent = Input;
+    });
+}
